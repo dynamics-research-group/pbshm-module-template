1e3ed3307e9dfd40feb4c1061bec03014e733443
TristanGowdridge <tristan.gowdridge@gmail.com>
2024-03-07 11:25:28 +0000

    Adding code block to make a sideloader.json

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import pathlib
+import json
 
 current_dir = pathlib.Path(os.getcwd())
 parent_dir = current_dir.parent.resolve()
@@ -22,3 +23,29 @@ os.rename("init_{{ cookiecutter.project_type }}.py", "__init__.py")
 # Remove redundant init file
 other_init = "framework" if "{{ cookiecutter.project_type}}" == "core" else "core"
 os.remove(f"init_{other_init}.py")
+
+{% if cookiecutter.project_type == "framework" %}
+
+side_loader_json = {
+    "package": {
+        "name": "{{ cookiecutter.project_slug }}",
+        "source": str(parent_dir),
+    },
+    "namespace": "{{ cookiecutter.project_namespace }}.{{ cookiecutter.project_slug }}",
+    "path": [
+        "{{ cookiecutter.project_namespace }}",
+        "{{ cookiecutter.project_slug }}",
+        "__init__.py"
+    ],
+    "url_prefix": "/{{ cookiecutter.project_slug }}",
+    "blueprint": True,
+    "navigation": {
+        "system": {
+            "Example Home": "{{cookiecutter.project_slug}}-home"
+        }
+    }
+}
+with open(os.path.join(str(current_dir), "{{ cookiecutter.project_slug }}_sideloader.json"), 'w') as f:
+    json.dump(side_loader_json, f, indent=4)
+
+{% endif %}
